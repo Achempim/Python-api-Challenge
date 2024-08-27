c0857b3cc076990e454afbf6e66864aee0d36606
George Achempim <achempim@gmail.com>
2024-08-27 05:25:47 -0400

    save the new fie

--- WeatherPy/Geo_app_key.py
+++
@@ -1,1 +0,0 @@
-Geo_app_key='9f87c6af97d143f19498819651d0d8ec'
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -3,6 +3,7 @@ from flask import app
 
 
 weather_api_key='831bce9438a9ff72298cdb00732fe4a7'
+Geo_app_key='9f87c6af97d143f19498819651d0d8ec'
 
 
 
