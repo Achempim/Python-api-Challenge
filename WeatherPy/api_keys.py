abb129b1ebd24a28afd0c55e42c878889392ae14
George Achempim <achempim@gmail.com>
2024-08-31 13:27:53 -0400

    change gitignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,9 +1,5 @@
 # OpenWeatherMap API Key
+weather_api_key = "831bce9438a9ff72298cdb00732fe4a7"
 
-
-
-weather_api_key='831bce9438a9ff72298cdb00732fe4a7'
-Geo_api_key='9f87c6af97d143f19498819651d0d8ec'
-
-
-
+# Geoapify API Key
+geoapify_key = "9f87c6af97d143f19498819651d0d8ec"
